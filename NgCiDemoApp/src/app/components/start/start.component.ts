9f6a9ab24b697af919ed99e73f7f03085f246abb
anuraglearnproject <anuraglearnproject@gmail.com>
2025-02-24 16:58:23 +0530

    resolve conflicts

--- NgCiDemoApp/src/app/components/start/start.component.ts
+++ NgCiDemoApp/src/app/components/start/start.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-start',
   imports: [RouterLink],
-=======
-
-@Component({
-  selector: 'app-start',
-  imports: [],
->>>>>>> 8c5a438be17b509ec2ba86320bd2f3f06ac52759
   templateUrl: './start.component.html',
   styleUrl: './start.component.css'
 })
